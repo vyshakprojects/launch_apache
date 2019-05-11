60faf651c875306d940ac428e122b06f562ce193
vyshakprojects <vyshakm117@gmail.com>
2019-05-11 11:49:44 +0000

    second

--- script.sh
+++ script.sh
@@ -7,6 +7,12 @@ echo "enter the server IP address:"
 read ip
 echo "the entered IP is : $ip"
 apt update
-apt install apache2
+apt-get install apache2 -y
+apache2 -v
+mv /var/www/html/index.html /var/www/html/index.html.bak
+cp -rv sitefiles/* /var/www/html/
 
+service apache2 start
+service apache2 enable
+service apache2 status
 
